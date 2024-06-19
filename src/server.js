0c171d3b4f8ff95b78a2c9efd3eea5b4ac694210
victor <victorgomesdacosta18@gmail.com>
2024-06-18 23:28:50 -0300

    feat

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-    return res.end('Hello world')
+    const { method, url } = req
+
+    if(method == 'GET' && url == '/users'){
+        return res.end(JSON.stringify(users))
+    }
+
+    if(method == 'POST' && url == '/users'){
+        users.push({
+            id: 1,
+            name: 'Victor Gomes',
+            email: 'victorgomes@example.com',
+        })
+        return res
+        .writeHead(201)
+        .end()
+    }
+
+    return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3333)
