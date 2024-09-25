a38dd1f29bf40e21f436696475cb28cb4e67174d
victor <victorgomesdacosta18@gmail.com>
2024-09-25 14:49:03 -0300

    feat:create id

--- src/database.js
+++ src/database.js
@@ -1,12 +1,40 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
+console.log(databasePath)
 
 export class Database {
-    database = {}
+    #database = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data)
+        })
+
+        .catch(() => {
+            this.#persist()
+        })
+    }
+
+    #persist() {
+        fs.writeFile('db.json', JSON.stringify(this.#database))
+    }
 
     select(table) {
+        const data = this.#database[table] ?? []
 
+        return data
     }
 
     insert(table, data) {
-        
+        if (Array.isArray(this.#database[table])) {
+            this.database[table].push(data)
+        } else {
+            this.#database[table] = [data]
+        }
+        this.#persist();
+
+        return data;
     }
 }
