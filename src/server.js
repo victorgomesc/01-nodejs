610f31e1d34d06e358c1daa888f60b160b762f79
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-11 00:35:15 -0300

    feat: create routes.js

--- src/server.js
+++ src/server.js
@@ -1,34 +1,21 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if(method == 'GET' && url == '/users'){
-        const users = database.select('users')
+    const route = routes.find(route => {
+        return route.method == method && route.path == url
+    })
 
-        return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+    if(route){
+        return route.handler(req, res)
     }
-
-    if(method == 'POST' && url == '/users'){
-        const { name, email} = req.body
-        const user = {
-            id: randomUUID(),  
-            name,
-            email,
-        }
-        database,insert('users', user)
-
-        return res.writeHead(201).end()
-    }
+    
     return res.writeHead(404).end()
     
 })
